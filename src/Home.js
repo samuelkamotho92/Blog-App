3165ff7a0bd4f1046f7717f1bc67ef2d9c222b25
samuel <samuelkamotho92@gmail.com>
2022-03-17 22:39:31 +0300

    updated the main root component

---
+++ src/Home.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-expressions */
+import React from 'react'
+
+import {useState,useEffect} from 'react'
+
+import BlogList from './blogsList';
+import useFecth from "./useFetch.js"
+
+const Home = () => {
+    const title = 'SamkamDev';
+   const {data:blogs,error,ispending} = useFecth();
+    return ( 
+        <div className="homepage">
+            <h1 style={{textAlign:"center"}}> Welcome to <span style={{color:" #f1356d"}}>{title}</span>  Projects Page</h1>
+            
+            {error && <div>{error}</div>}
+            {ispending && <div>Loading...</div>}
+            
+            {/* jsx inside the js always inside a parathesis */}
+   {blogs &&  <BlogList blogs={blogs} project="projects" />}
+        </div>
+    
+ 
+     );
+}
+ 
+export default Home;
